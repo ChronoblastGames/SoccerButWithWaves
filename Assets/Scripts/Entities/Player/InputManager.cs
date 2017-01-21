9b9d7930973785438194c94b1770cdee6d604a70
Matthew Ferreira <ChronoblastGamesOfficial@gmail.com>
2017-01-20 21:49:37 -0500

    Initial Commit of Project Files; Added Main Scene, Added InputManager, PlayerController. Added Base_Color Materials.

---
+++ Assets/Scripts/Entities/Player/InputManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    private PlayerController playerController;
+
+    [Header("Player Info")]
+    public string playerNumber;
+
+    [Header("Player Controls")]
+    public float xAxis;
+    public float yAxis;
+
+    public KeyCode Interact;
+    public KeyCode Pause;
+
+    public bool canMove;
+    public bool canGetInput;
+
+	void Start ()
+    {
+        GetPlayerInfo();
+	}
+	
+	void Update ()
+    {
+        GetInput();
+	}
+
+    void GetPlayerInfo()
+    {
+        playerNumber = tag;
+
+        playerController = GetComponent<PlayerController>();
+    }
+
+    void GetInput()
+    {
+        if (canGetInput)
+        {
+            if (canMove)
+            {
+                xAxis = Input.GetAxisRaw("Horizontal");
+
+                yAxis = Input.GetAxisRaw("Vertical");
+            }
+            else
+            {
+                xAxis = 0;
+                yAxis = 0;
+            }
+
+            if (Input.GetKeyDown(Pause))
+            {
+
+            }
+
+            if (Input.GetKeyDown(Interact))
+            {
+
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Entities/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof (InputManager))]
+[RequireComponent(typeof (Rigidbody))]
+
+public class PlayerController : MonoBehaviour
+{
+    private InputManager inputManager;
+
+    private Rigidbody myRB;
+
+    [Header("Player Movement Attributes")]
+    private float xAxis;
+    private float yAxis;
+    public float moveSpeed;
+    private float baseMoveSpeed;
+
+    [Header("Player Stamina Attributes")]
+    public float currentStamina;
+    public float staminaRegenerationRate;
+    public float staminaCostPerSecond;
+    
+	void Start ()
+    {
+        PlayerSetup();
+	}
+	
+	void FixedUpdate ()
+    {
+        GatherInputs();
+	}
+
+    void PlayerSetup()
+    {
+        myRB = GetComponent<Rigidbody>();
+
+        inputManager = GetComponent<InputManager>();
+    }
+
+    void GatherInputs()
+    {
+        xAxis = inputManager.xAxis;
+        yAxis = inputManager.yAxis;
+
+        if (xAxis != 0 || yAxis != 0)
+        {
+            Move(xAxis, yAxis);
+        }
+    }
+
+    void Move(float xAxis, float yAxis)
+    {
+        if (xAxis != 0)
+        {
+            myRB.AddForce(Vector3.right * xAxis * moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        }
+
+        if (yAxis != 0)
+        {
+            myRB.AddForce(Vector3.forward * yAxis * moveSpeed * Time.deltaTime, ForceMode.Impulse);
+        }
+    }
+}
