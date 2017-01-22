d82cf7661c35fab9475c40938740a21575434824
Remember that guy with the fun and caring personality? That used to be me! <stevenramos0615@gmail.com>
2017-01-22 08:39:16 -0500

    TitleScreen now impulses

---
+++ Assets/Scripts/System/TextImpulse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextImpulse : MonoBehaviour
+{
+    void Update()
+    {
+        transform.localScale = new Vector3(Mathf.Lerp(transform.localScale.x, Mathf.PingPong(Time.time,1f), Time.deltaTime), Mathf.Lerp(transform.localScale.y, Mathf.PingPong(Time.time, 1f), Time.deltaTime), 0f);
+    }
+}
