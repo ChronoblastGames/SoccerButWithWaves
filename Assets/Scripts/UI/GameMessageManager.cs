931ae1ae7077f301847efd581e168a5da48dd06b
Phlares <ryon.everett@gmail.com>
2017-01-21 23:10:59 -0500

    Added Time scale, game freeze on time out game manager
    Hai

---
+++ Assets/Scripts/UI/GameMessageManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMessageManager : MonoBehaviour {
+
+	//SendToEndGameMenu;
+	public Text textToDisplay;
+	public string redTeamWins;
+	public string blueTeamWins;
+	public string drawText;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void DisplayMessage(string messageToSend){
+		switch(messageToSend){
+		case "RedWin":
+			textToDisplay.text = redTeamWins;
+			break;
+		case "BlueWin":
+			textToDisplay.text = blueTeamWins;
+			break;
+		case "Draw":
+			textToDisplay.text = drawText;
+			break;
+		default:
+			break;
+		}
+
+	}
+}
---
+++ Assets/Scripts/UI/UIMaster.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMaster : MonoBehaviour {
+
+	[Header ("UI Elements")]
+	public Text redTeamScoreUI;
+	public Text blueTeatScoreUI;
+	public Text scoreDisplay;
+
+	[Header ("Find The Timer")]
+	public GameObject scriptThatDoesTimerGO;
+
+	private int timeDisplay;
+	private
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		timeDisplay = scriptThatDoesTimerGO.GetComponent<ScriptThatJustManagesTheTimeThatsIt> ().timeLeft;
+		string minutes = Mathf.Floor(timeDisplay / 60).ToString("00");
+		string seconds = (timeDisplay % 60).ToString("00");
+		scoreDisplay.text = (minutes +  ":" + seconds);
+	}
+}
